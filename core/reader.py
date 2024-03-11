4e1177c2ae824201cbffd1198b08ce9f2ff32ef1
v1ntoka <galimov64946@gmail.com>
2024-03-11 11:41:20 +0300

    Все фильтры завершены, приступаю к тестированию логики

--- core/reader.py
+++ core/reader.py
@@ -6,7 +6,8 @@ from upload.models import UploadModel
 from django.utils import timezone
 
 
-def _get_file(filename: str):
+def _get_file(filename: str) -> UploadModel.File:
+    """Возвращает файл из базы данных по имени, попутно обновляя дату-время последнего открытия"""
     file = UploadModel.objects.get(name=filename)
     file.date = timezone.now()
     file.save(date_update=True)
@@ -15,12 +16,13 @@ def _get_file(filename: str):
 
 class Reader:
     def __init__(self, filename: str, *args, **kwargs):
-        self.file = _get_file(filename)
-        self.text = [i.decode('utf-8') for i in self.file.File.readlines()]
+        self.file: UploadModel.File = _get_file(filename)
+        self.text: list[str] = [i.decode('utf-8') for i in self.file.File.readlines()]
         self.args = args
         self._kwargs_processing(kwargs)
 
     def _kwargs_processing(self, kwargs) -> None:
+        """Приводит кварги к более удобной для взаимодействия форме"""
         for key in kwargs:
             if key != 'csrfmiddlewaretoken':
                 if key in ('date_before', 'date_after') and kwargs[key] != ['']:
@@ -28,23 +30,81 @@ class Reader:
                 else:
                     setattr(self, key, kwargs[key][0].strip())
 
-    def _extra_filter(self):
-        res = []
+    def _extra_filter(self) -> None:
+        """Выполняется приоритетнее всего.
+        Включает в выдачу только строки, в которых сработало введенное регулярное выражение"""
+        res: list[str] = []
         for line in self.text:
             if re.search(getattr(self, 'extra', ''), line):
                 res.append(line)
         self.text = res
 
-    def _plate_filter(self):
-        ...
+    def _plate_filter(self) -> None:
+        """Само по себе не меняет выдачу, лишь настраивает менее приоритетные фильтры.
+        Если фильтры были проставлены вручную(например дата и время), то сначала отработают они,
+        только потом выставятся новые"""
+        match: int = 0
+        res: list[str] = []
 
-    def _date_filter(self):
-        ...
+        if any((getattr(self, 'date_before', None), getattr(self, 'date_after', None))):
+            self._date_filter()
+        if getattr(self, 'entrance', None):
+            self._entrance_filter()
 
-    def _entrance_filter(self):
-        ...
+        for line in self.text:
+            if re.search(getattr(self, 'plate', ''), line):
+                # Запоминаем, где найдена машина
+                search = re.search(Patterns.entrance, line)
+                if search is not None:
+                    setattr(self, 'entrance', search.group('entrance'))
+        if getattr(self, 'entrance', None):
+            # Во всех логах ищем те, которые связаны исключительно с въездом-выездом, где найдена машина
+            self._entrance_filter()
+            for index, line in enumerate(self.text):
+                if re.search(getattr(self, 'plate', ''), line):
+                    # Запоминаем, по какому индексу обнаружен номер
+                    match = index
+            for line in self.text[match:0:-1]:
+                res.append(line)
+                if re.search(Patterns.loop_a, line):
+                    # Добавляем все логи выше найденного индекса пока не сработает петля А
+                    break
+            res = res[::-1]
+            for line in self.text[match + 1:]:
+                res.append(line)
+                if re.search(Patterns.loop_b, line):
+                    # аналогично предыдущему шагу, но с петлей Б
+                    break
+        self.text = res
 
-    def read(self, *args, **kwargs) -> list[str]:
+    def _date_filter(self) -> None:
+        res: list[str] = []
+        if getattr(self, 'date_before', None) and getattr(self, 'date_after', None):
+            for line in self.text:
+                if (search := re.search(Patterns.datetime, line)) and (
+                        getattr(self, 'date_before') <= dt.datetime.fromisoformat(
+                        search.group('datetime')) <= getattr(self, 'date_after')):
+                    res.append(line)
+        elif getattr(self, 'date_before', None):
+            for line in self.text:
+                if (search := re.search(Patterns.datetime, line)) and  getattr(self, 'date_before') <= dt.datetime.fromisoformat(
+                        search.group('datetime')):
+                    res.append(line)
+        elif getattr(self, 'date_after', None):
+            for line in self.text:
+                if (search := re.search(Patterns.datetime, line)) and getattr(self, 'date_after') <= dt.datetime.fromisoformat(
+                        search.group('datetime')):
+                    res.append(line)
+        self.text = res
+
+    def _entrance_filter(self) -> None:
+        res: list[str] = []
+        for line in self.text:
+            if (search := re.search(Patterns.entrance, line)) and (search.group('entrance') == getattr(self, 'entrance')):
+                res.append(line)
+        self.text = res
+
+    def read(self) -> list[str]:
         if getattr(self, 'extra', None):
             self._extra_filter()
         if getattr(self, 'plate', None):
--- core/regex.py
+++ core/regex.py
@@ -1,10 +1,8 @@
-import re
-
-
 class Patterns:
-    datetime = r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}"
+    datetime = r"^(?P<datetime>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3})"
     plate = r"plate recognized: (?P<plate>\S+) "
     entrance = r"[\S]*(?P<entrance>В[ы]?х\d*)[\S]*"
     datetime_entrance = r"(?P<datetime>^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}).* [\S]*(?P<entrance>В[ы]?х\d*)[\S]*"
     datetime_plate = r"(?P<datetime>^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}).* plate recognized: (?P<plate>\S+) "
-
+    loop_a = r"ПетляА|ПетА"
+    loop_b = r"ПетляБ|ПетБ"
