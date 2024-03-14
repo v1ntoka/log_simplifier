f538c61c9b773b7850b8164b22ab54a2a2161e83
v1ntoka <galimov64946@gmail.com>
2024-03-14 11:36:29 +0300

    release

--- reader/views.py
+++ reader/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
-# from core.handlers import actualize_filename
 from core.reader import Reader
 from reader.forms import Filters
 from django.shortcuts import redirect
@@ -10,7 +9,6 @@ def reader(request, filename=None):
     if not filename:
         return redirect('upload:upload')
     log_reader = Reader(filename=filename, **request.POST)
-    # filters = Filters(request.POST)
     filters = Filters(log_reader.filters)
     context = {'filters': filters, 'filename': filename}
     if filters.is_valid():
